e37a8dc34f6b83597b0c80625ee9171cac258b88
Roman S. Nesterov <galeopsis@mail.ru>
2023-08-02 13:34:56 +0700

    version 1.1 fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.galeopsis.pinger"
         minSdk = 24
         targetSdk = 33
-        versionCode = 1
+        versionCode = 2
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
